c086e5cf300cef9da190e3861284a55f65e5f0d6
Jari De Wulf <jari.de.wulf@student.howest.be>
2021-09-23 23:14:57 +0200

    A user can host and join a game

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,91 @@ import "./App.css";
 
 function App() {
   const socketRef = useRef();
+  const [nameInput, setNameInput] = useState("");
+  const [playerCodeInput, setPlayerCodeInput] = useState("");
+  const [gameCodeInput, setGameCodeInput] = useState("");
 
   useEffect(() => {
     socketRef.current = io.connect("http://localhost:80/");
 
     socketRef.current.on("connect", () => {
-      console.log("connected");
+      console.log(socketRef.current.id);
     });
   }, []);
+
+  const onSubmitPlayer = (event) => {
+    event.preventDefault();
+    if (
+      nameInput ||
+      nameInput.trim ||
+      nameInput.trim().length !== 0 ||
+      playerCodeInput ||
+      playerCodeInput.trim ||
+      playerCodeInput.trim().length !== 0
+    ) {
+      let player = {
+        id: socketRef.current.id,
+        name: nameInput,
+        game: playerCodeInput,
+      };
+      socketRef.current.emit("player", player);
+      setNameInput("");
+      setPlayerCodeInput("");
+      console.log("player sent:", player);
+    }
+  };
+
+  const onSubmitHost = (event) => {
+    event.preventDefault();
+    console.log("set up game with code: ", gameCodeInput);
+    if (gameCodeInput !== "") {
+      socketRef.current.emit("code", gameCodeInput);
+      setGameCodeInput("");
+    }
+  };
 
   return (
     <div className="App">
       <h1>Welcome to the app!</h1>
+      <h2>User form</h2>
+      <form onSubmit={onSubmitPlayer}>
+        <label htmlFor="name">Username</label>
+        <br />
+        <input
+          type="text"
+          value={nameInput}
+          onChange={(event) => {
+            setNameInput(event.target.value);
+          }}
+        />
+        <br />
+        <label htmlFor="name">Code</label>
+        <br />
+        <input
+          type="text"
+          value={playerCodeInput}
+          onChange={(event) => {
+            setPlayerCodeInput(event.target.value);
+          }}
+        />
+        <br />
+        <button type="submit">Join</button>
+      </form>
+
+      <h2>Host form</h2>
+      <form onSubmit={onSubmitHost}>
+        <label htmlFor="name">Code</label>
+        <br />
+        <input
+          type="text"
+          value={gameCodeInput}
+          onChange={(event) => {
+            setGameCodeInput(event.target.value);
+          }}
+        />
+        <br />
+        <button type="submit">Host</button>
+      </form>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -10,12 +10,56 @@ const io = require("socket.io")(server, {
   },
 });
 
+let games = [];
+
 io.on("connection", (socket) => {
   console.log("Socket connected", socket.id);
 
-  socket.on("name", (name) => {
-    console.log("name entered:", name);
-    io.sockets.emit(`name`, name);
+  //When a socket receives a player object
+  socket.on("player", (player) => {
+    let exists = false;
+    //Check if the room exists that the player is trying to join
+    games.forEach((game) => {
+      if (game.code === player.game) {
+        game.players.forEach((playerToCheck) => {
+          //Check for duplicate names in the game
+          if (playerToCheck.name === player.name) {
+            exists = true;
+          }
+        });
+        //If not duplicate
+        if (!exists) {
+          game.players.push(player);
+          console.log(
+            `${player.name} is succesfully pushed to game ${game.code}`
+          );
+          console.log(games);
+        }
+      }
+    });
+  });
+
+  //When a socket receives a code
+  socket.on("code", (code) => {
+    console.log("code entered:", code);
+    io.sockets.emit(`code`, code);
+
+    let exists = false;
+
+    //Check if a game exists with that code, if it doesn't create the game
+    //TODO: (Maybe use find or dictionary)
+    games.forEach((game) => {
+      if (game.code === code) {
+        console.log("game already exists");
+        exists = true;
+        return;
+      }
+    });
+
+    if (!exists) {
+      games.push({ id: socket.id, code: code, players: [] });
+      console.log(games);
+    }
   });
 });
 
