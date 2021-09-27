91ec4c52eb1d705e9d755423c8d35e6249565f24
Jari De Wulf <jari.de.wulf@student.howest.be>
2021-09-27 19:14:40 +0200

    Add hard coded url to check

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: process.env.CLIENT,
+    origin: "https://challenge-1-communication-client.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
