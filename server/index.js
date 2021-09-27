7bcd968b28ee25b7decd34059d6e8d743ec74f1f
Jari De Wulf <jari.de.wulf@student.howest.be>
2021-09-27 19:22:39 +0200

    Update origin

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: "https://challenge-1-communication-client.vercel.app/",
+    origin: "https://challenge-1-communication-client.vercel.app",
     credentials: true,
     methods: ["GET", "POST"],
   },
